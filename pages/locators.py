a5a9b32bced9052c89fcf17e76b72a12ade84bd8
Nikidiplumbah <nikki.tosah@mail.ru>
2025-05-08 20:00:18 +0200

    base, locators and main modified

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,15 @@
 from selenium.webdriver.common.by import By
 
-
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+    
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    ADDED_PRODUCT_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-success:nth-child(1) strong")
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
