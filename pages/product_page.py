01218b86f13efbb3e10cbc46dcf1368f14a2993a
Nikidiplumbah <nikki.tosah@mail.ru>
2025-05-08 20:01:24 +0200

    product_page added

---
+++ pages/product_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_button.click()
+        
+    def should_be_add_to_busket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Add to basket button is not present"
+        
+    def should_be_correct_product_added(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        success_message = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_MESSAGE).text
+        assert product_name == success_message, f"Expected '{product_name}', got '{success_message}'"
+        
+    def should_be_correct_basket_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
+        assert product_price == basket_total, f"Expected basket total '{product_price}', got '{basket_total}'"
