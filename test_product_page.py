be965002846584ce8809ea89fbecef4e3ca1111d
Nikidiplumbah <nikki.tosah@mail.ru>
2025-05-13 20:44:15 +0200

    pytest.ini, test_product modified

--- test_product_page.py
+++ test_product_page.py
@@ -33,7 +33,8 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is displayed for new user without adding product"
-
+    
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
         page = ProductPage(browser, link)
@@ -42,7 +43,8 @@ class TestUserAddToBasketFromProductPage():
         page.solve_quiz_and_get_code()
         page.should_be_correct_product_added()
         page.should_be_correct_basket_price()
-    
+
+@pytest.mark.need_review    
 def test_guest_can_add_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
@@ -60,7 +62,6 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
     
 
-@pytest.mark.need_review
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link ='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
@@ -82,13 +83,15 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-    
+
+@pytest.mark.need_review    
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
-    
+
+@pytest.mark.need_review    
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
